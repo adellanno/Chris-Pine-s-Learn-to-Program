58732ad94abfca48b853c1de3e6b3344eb13f427
adellanno <anthony@dellanno.co.uk>
2015-08-17 15:41:42 +0000

    Excercise 5.6 Full Name Greeting

---
+++ ch5-mixing-it-up/ex5-6.rb
@@ -0,0 +1,7 @@
+puts "Hello sir, what is your first name please?"
+firstName = gets.chomp
+puts "Thank you " + firstName + ". May I now have your middle name please?"
+middleName = gets.chomp
+puts "Thats great. And finally your last name please."
+lastName = gets.chomp
+puts "OK, " + firstName + " " + middleName + " " + lastName + ", that's all I need, thank you!"
