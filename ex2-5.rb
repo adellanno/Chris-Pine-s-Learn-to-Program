d6312b6cbd420be5e852607d3919a8c8c105bdc9
adellanno <anthony@dellanno.co.uk>
2015-08-17 13:51:42 +0000

    Exercise 2.5

---
+++ ex2-5.rb
@@ -0,0 +1,11 @@
+#How many hours are in a year?
+puts 24 * 365
+
+#How many minutes are in a decade?
+puts 10 * 365 * 24 * 60
+
+#How many seconds old am I?
+puts 25 * 365 * 24 * 60 * 60 + (228 * 24 * 60 * 60)
+
+#If I am 1.160 million seconds old. How old am I?
+puts 1160000000.0 / (365.0 * 24.0 * 60.0 * 60.0)
