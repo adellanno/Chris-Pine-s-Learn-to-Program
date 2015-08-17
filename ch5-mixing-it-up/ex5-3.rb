fab948b25c1a6bd072b4edd228e1801b5dde43a0
adellanno <anthony@dellanno.co.uk>
2015-08-17 15:28:50 +0000

    Exercise 5.3 The gets method

---
+++ ch5-mixing-it-up/ex5-3.rb
@@ -0,0 +1,8 @@
+puts gets
+
+# If puts means put string, I’m sure you can guess what gets stands for. And just
+# as puts always spits out strings, gets retrieves only strings. And whence does
+# it get them?
+# From you! Well, from your keyboard, anyway. And since your keyboard makes
+# only strings, that works out beautifully. What actually happens is that gets
+# just sits there, reading what you type until you press enter
