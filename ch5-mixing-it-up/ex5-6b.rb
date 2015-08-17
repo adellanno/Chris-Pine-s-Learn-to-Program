4a582201ba55850742d769569784593ce4d1eeec
adellanno <anthony@dellanno.co.uk>
2015-08-17 15:52:29 +0000

    Exercise 5.6b Bigger, better favourite number

---
+++ ch5-mixing-it-up/ex5-6b.rb
@@ -0,0 +1,3 @@
+puts "Good day to you! What is your favourite number please?"
+number = gets.chomp.to_i
+puts "I see, you must think bigger! Don't you think that " + (number + 1000).to_s + " is a better number?"
