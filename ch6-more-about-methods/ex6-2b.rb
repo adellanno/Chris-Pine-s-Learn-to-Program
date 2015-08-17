061df48858b6317b85ee7e9811d1fc6c12f3bf8e
adellanno <anthony@dellanno.co.uk>
2015-08-17 16:43:00 +0000

    Exercise 6.2b Tables of Contents

---
+++ ch6-more-about-methods/ex6-2b.rb
@@ -0,0 +1,6 @@
+lineWidth = 40
+
+puts ('Table of Contents'.center(lineWidth))
+puts ('Chapter 1: Learn'.ljust(lineWidth/2)) + ('Page 1'.rjust(lineWidth/2))
+puts ('Chapter 2: Apply'.ljust(lineWidth/2)) + ('Page 2'.rjust(lineWidth/2))
+puts ('Chapter 3: Earn'.ljust(lineWidth/2)) + ('Page 3'.rjust(lineWidth/2)) 
